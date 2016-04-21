cd84cb3fef4c58cc1093b635fb48e120cb647d23
carvbapt <carvbapt@gmail.com>
2016-04-21 22:53:44 +0100

    Borders

---
+++ FingerGame/Assets/_Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Translate (new Vector3(Input.GetAxis("Horizontal")*speed,Input.GetAxis("Vertical")*speed,0)*Time.deltaTime);
+	}
+}
--- FingerGame/Assets/_Scripts/Scroll.cs
+++ FingerGame/Assets/_Scripts/Scroll.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Scroll : MonoBehaviour {
 
+	public float speed = 5f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,18 @@ public class Scroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Vector2 offset = new Vector2 (Time.time * speed, 0);
+
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x += Time.fixedDeltaTime / speed;
+
+		mat.mainTextureOffset = offset;
 	
 	}
 }
